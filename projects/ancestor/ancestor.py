9a149b86e0cb73c206affdb50fde57f4542283a4
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-04-14 13:18:19 -0700

    passes tests

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,28 @@
+def get_parents(ancestors, child):
+    parents = []
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+    for node in ancestors:
+        if node[1] == child:
+            parents.append(node[0])
+    
+    return parents
+
+def earliest_ancestor(ancestors, starting_node, path=None):
+    parents = get_parents(ancestors, starting_node)
+
+    if path is None:
+        path = []
+    
+    if not len(parents) and not len(path):
+        return -1
+    
+    new_path = [*path, starting_node]
+
+    if not len(parents):
+        return starting_node
+
+    for parent in parents:
+        ea = earliest_ancestor(ancestors, parent, new_path)
+
+        if ea is not None:
+            return ea
