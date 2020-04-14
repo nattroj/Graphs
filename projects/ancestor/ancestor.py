46c28c08213f55a83cdba0a2dbdebf9b5130c26f
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-04-14 16:49:36 -0700

    removes commented code

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -45,6 +45,3 @@ def earliest_ancestor(ancestors, starting_node, path=None):
     results = map(lambda ancestor: ancestor[1], earliest_ancestors)
 
     return min(results)
-
-# test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-# print(earliest_ancestor(test_ancestors, 10))
