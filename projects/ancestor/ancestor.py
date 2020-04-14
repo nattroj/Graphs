978af6153a3dca25688867002e1b0f1f7d74daac
Nattajohn Rojanasupya <n.rojanasupya@gmail.com>
2020-04-14 16:25:17 -0700

    updates function to account for edge cases where there are multiple "earliest ancesttors"

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -8,21 +8,34 @@ def get_parents(ancestors, child):
     return parents
 
 def earliest_ancestor(ancestors, starting_node, path=None):
-    parents = get_parents(ancestors, starting_node)
+    def recurse(ancestors, starting_node, path=None, early_ancestors=None):
+        parents = get_parents(ancestors, starting_node)
 
-    if path is None:
-        path = []
-    
-    if not len(parents) and not len(path):
+        if path is None:
+            path = []
+        
+        if not len(parents) and not len(path):
+            return False
+        
+        new_path = [*path, starting_node]
+
+        if not len(parents):
+            return (len(new_path), new_path[-1])
+
+        for parent in parents:
+            ea = recurse(ancestors, parent, new_path, early_ancestors=early_ancestors)
+
+            if ea is not None:
+                early_ancestors.append(ea)
+
+    early_ancestors = []
+
+    if recurse(ancestors, starting_node, early_ancestors=early_ancestors) is False:
         return -1
-    
-    new_path = [*path, starting_node]
+        
+    paths = map(lambda ancestor: ancestor[0], early_ancestors)
+    longest_path = max(paths)
 
-    if not len(parents):
-        return starting_node
+    earliest_ancestors = filter(lambda ancestor: ancestor[0] == longest_path, early_ancestors)
 
-    for parent in parents:
-        ea = earliest_ancestor(ancestors, parent, new_path)
-
-        if ea is not None:
-            return ea
+    return min(map(lambda ancestor: ancestor[1], earliest_ancestors))
